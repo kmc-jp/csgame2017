25c63403369131559fabc20bca819bb32e31b220
ten986 <tenkichi986@yahoo.co.jp>
2017-05-13 13:02:24 +0900

    Add files via upload

---
+++ csgame4/template.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//おまじない
+using asd;
+
+namespace Altseedtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //初期化処理
+            //タイトル名、幅、高さ、オプション
+            Engine.Initialize("STG", 640, 480, new EngineOption());
+            
+            //ウィンドウが閉じられていなければ、while内を実行
+             while (Engine.DoEvents())
+             {
+                //更新処理
+                Engine.Update();
+             }
+
+            //終了処理
+            Engine.Terminate();
+        }
+    }
+}
